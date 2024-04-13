b4661c77d621f044c8930b77858b86d2bd738c91
julio <julio.sanic.gt.256@gmail.com>
2024-04-12 20:08:14 -0600

    image slider finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,33 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import ImageSlider from "@/components/image-slider/ImageSlider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dispositivos Electrónicos',
+  description: 'Venta de dispositivos electrónicos a precios accesibles',
+  keywords: 'electronica, dispositivos, esp8266, arduino, raspberry pi',
+  openGraph: {
+    title: 'Dispositivos Electrónicos',
+    description: 'Venta de dispositivos electrónicos a precios accesibles',
+    type: 'website',
+    url: 'https://electronica.com',
+    images: [
+      {
+        url: 'https://julius-assets-gt.s3.amazonaws.com/pi_zero.jpg',
+        width: 800,
+        height: 533,
+        alt: 'Raspberry Pi Zero',
+      }
+    ],
+    locale: 'es_GT',
+    countryName: 'Guatemala',
+
+  }, 
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <section className="container">
+      <ImageSlider />
+    </section>
+  )
 }
